17fbdc942c1e00d61c239ad55f5339064543792d
Alex Velasquez <jtinnesco@gmail.com>
2019-12-25 23:23:57 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "students", force: :cascade do |t|
+  create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.string "size"
